3ef77976b1ffaeaa979cd77faff54a3c34fd4d91
vaishbil <vaishaliwankhede777@gmail.com>
2026-08-18 11:50:55 +0530

    added comments

--- server/utils/generateRoomCode.js
+++ server/utils/generateRoomCode.js
@@ -1,4 +1,4 @@
-// Generates a short, human-friendly room code like "X7K2A9"
+// Generates a short room code like "X7K2A9"
 export function generateRoomCode(length = 6) {
   const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"; // no 0/O/1/I to avoid confusion
   let code = "";
